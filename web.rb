f7331c1784af6a046a9eab3466aa8fe945021b7b
Nathan Perkins <nperkins487@gmail.com>
2016-03-28 22:19:33 +0000

    add image links from imgur

--- web.rb
+++ web.rb
@@ -3,6 +3,8 @@ require 'tilt/erubis'
 require 'yaml'
 require 'rack'
 require 'rack/ssl'
+require 'open-uri'
+require 'json'
 
 use Rack::SSL
 
@@ -15,17 +17,26 @@ configure do
   set :erb, :escape_html => true
 end
 
+def image_links_from_imgur
+  secret_data = YAML.load_file('data/secret_authentication.yml')
+  album_id = secret_data['imgur']['album']
+  api_url = "https://api.imgur.com/3/album/#{album_id}/images"
+  authorization_header = 'Bearer 482ffa280b7cc4d1c06646965e082ec82fc30407'
+  imgur_data = JSON.parse open(api_url, 'Authorization' => authorization_header).string
+  imgur_data['data'].map { |image| image['link'] }
+end
+
 before do
-  @data = YAML.load_file('public/dogs.yml')
+  @image_links = image_links_from_imgur
 end
 
 get '/' do
-  @dog_link = @data['dogs'].sample
+  @dog_link = @image_links.sample
   erb :index
 end
 
 get '/gif' do
-  @dog_link = @data['dogs'].sample
+  @dog_link = @image_links.sample
 end
 
 get '/*' do
